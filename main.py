a4217559e51ce5de2cbbfe112b2864df05cb8fda
huey-J <wkdgus7113@gmail.com>
2023-01-25 13:32:27 +0900

    refactor: make 1 sql file per 1 year

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
 BASE_URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/"
 ANNIVERSARY_URL = "getAnniversaryInfo"
 NATIONAL_HOLIDAY_URL = "getHoliDeInfo"
 EXCLUDED_SEASON_URL = "getSundryDayInfo"
 SEASON_URL = "get24DivisionsInfo"
 
-SERVICE_KEY = "INPUT SECRET KEY"
+SERVICE_KEY = "{{ INPUT YOUR SECRET KEY }}"
 NUM_OF_ROWS = "100"
 
 
@@ -26,9 +25,13 @@ def parseDateKind(dateKind):
         return ""
 
 
-def getHolidayData(url, solYear):
+def getHolidayData(url, solYear, f):
     # GET Data
     FULL_URL = BASE_URL + url + "?solYear=%s&serviceKey=%s&numOfRows=%s" % (solYear, SERVICE_KEY, NUM_OF_ROWS)
+    if SEASON_URL == url:
+        kst = "0120"
+        sunLongitude = "285"
+        FULL_URL += "&kst=%s&sunLongitude=%s" % (kst, sunLongitude)
     response = requests.get(FULL_URL)
 
     # XML Parsing
@@ -36,37 +39,7 @@ def getHolidayData(url, solYear):
     items = soup.find_all("item")
 
     # Create SQL File
-    f = open(solYear + '_' + url + '.sql', 'w')
-    f.write("-- 총 %d개\n" % len(items))
-    f.write("INSERT INTO `holiday` (`holiday_type`, `date_name`, `holiday`, `date`, `created_date_time`, `updated_date_time`)\n\tVALUES\n")
-
-    item_str = []
-    for item in items:
-        dateKind = parseDateKind(item.find("dateKind").get_text())
-        dateName = item.find("dateName").get_text()
-        isHoliday = 1 if item.find("isHoliday").get_text() == "Y" else 0
-        locdate = datetime.strptime(item.find("locdate").get_text(), '%Y%m%d').strftime("%Y-%m-%d")
-        # seq = item.find("seq").get_text()
-        item_str.append("\t\t('%s', '%s', %s, '%s', now(), now())\n" % (dateKind, dateName, isHoliday, locdate))
-    f.write(", ".join(item_str))
-    f.write(";")
-    f.close()
-
-
-def getSeasonData(url, solYear):
-    kst = "0120"
-    sunLongitude = "285"
-    # GET Data
-    FULL_URL = BASE_URL + url + "?solYear=%s&serviceKey=%s&numOfRows=%s&kst=%s&sunLongitude=%s" % (solYear, SERVICE_KEY, NUM_OF_ROWS, kst, sunLongitude)
-    response = requests.get(FULL_URL)
-
-    # XML Parsing
-    soup = BeautifulSoup(response.text, 'lxml-xml')
-    items = soup.find_all("item")
-
-    # Create SQL File
-    f = open(solYear + '_' + url + '.sql', 'w')
-    f.write("-- 총 %d개\n" % len(items))
+    f.write("-- %s 총 %d개\n" % (url, len(items)))
     f.write(
         "INSERT INTO `holiday` (`holiday_type`, `date_name`, `holiday`, `date`, `created_date_time`, `updated_date_time`)\n\tVALUES\n")
 
@@ -79,15 +52,16 @@ def getSeasonData(url, solYear):
         # seq = item.find("seq").get_text()
         item_str.append("\t\t('%s', '%s', %s, '%s', now(), now())\n" % (dateKind, dateName, isHoliday, locdate))
     f.write(", ".join(item_str))
-    f.write(";")
-    f.close()
+    f.write(";\n\n")
 
 
 def holiday_crawler(solYear):
-    getHolidayData(ANNIVERSARY_URL, solYear)
-    getHolidayData(NATIONAL_HOLIDAY_URL, solYear)
-    getHolidayData(EXCLUDED_SEASON_URL, solYear)
-    getSeasonData(SEASON_URL, solYear)
+    f = open('holiday_info' + '_' + solYear + '.sql', 'w')
+    getHolidayData(ANNIVERSARY_URL, solYear, f)
+    getHolidayData(NATIONAL_HOLIDAY_URL, solYear, f)
+    getHolidayData(EXCLUDED_SEASON_URL, solYear, f)
+    getHolidayData(SEASON_URL, solYear, f)
+    f.close()
 
 
-holiday_crawler("2024")
+holiday_crawler("2023")
